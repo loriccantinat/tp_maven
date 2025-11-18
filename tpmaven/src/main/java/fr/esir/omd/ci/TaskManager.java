27df930dcc79079fb61fb442880097316f4e3fd8
Loric <loric.cantinat@etudiant.univ-rennes.fr>
2025-11-18 17:02:00 +0100

    jvfx et plus : étape après le checkstyle

---
+++ tpmaven/src/main/java/fr/esir/omd/ci/TaskManager.java
@@ -0,0 +1,31 @@
+package fr.esir.omd.ci;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class TaskManager {
+    private List<Task> tasks;
+    private static final Logger logger = LoggerFactory.getLogger(TaskManager.class);
+
+
+    public TaskManager() {
+        tasks = new ArrayList<>();
+        logger.info("TaskManager initialized");
+    }
+
+    /* Ceci est un commentaire pour la méthode addTask */
+    public void addTask(Task task) {
+        tasks.add(task);
+        logger.info("Added task: {}", task.getTitle());
+    }
+
+    /* Ceci est un commentaire pour la méthode getTasks */
+    public List<Task> getTasks() {
+        logger.debug("Retrieving all tasks, count: {}", tasks.size());
+        return tasks;
+    }
+}
+
